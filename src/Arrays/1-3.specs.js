b5d5e1743804f5ef72b807b4597183fd246f38b6
Jack Jiang <thejackjiang@gmail.com>
2017-11-21 17:55:04 -0500

    fixed: fixed bug in 1-3 to work for all cases

--- src/Arrays/1-3.specs.js
+++ src/Arrays/1-3.specs.js
@@ -4,11 +4,11 @@ describe('the function `spaces`,', () => {
     expect(spaces('Mr John Smith   ', 13).length).toEqual(17)
   });
   it('replaces spaces in the string with %20, I am batman', function() {
-    expect(spaces('I am batman   ', 13)).toEqual('I%20am%20batman');
-    expect(spaces('I am batman   ', 13).length).toEqual(15)
+    expect(spaces('I am batman   ', 14)).toEqual('I%20am%20batman');
+    expect(spaces('I am batman   ', 14).length).toEqual(15)
   });
   it('replaces spaces in the string with %20, even string of spaces', function() {
-    expect(spaces('     ', 13)).toEqual('%20%20%20%20');
-    expect(spaces('     ', 13).length).toEqual(12)
+    expect(spaces('     ', 14)).toEqual('%20%20%20%20');
+    expect(spaces('     ', 14).length).toEqual(12)
   });
 });
