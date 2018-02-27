0b14def7ccc750d7e1099db9d58df971c3a19e10
Jack Jiang <thejackjiang@gmail.com>
2018-02-27 18:57:02 -0500

    feat: comments for 2.21

--- src/LinkedLists/2-2.js
+++ src/LinkedLists/2-2.js
@@ -11,3 +11,9 @@ const fromEnd = (num, node) => {
   return first.data
 }
 
+/*
+finding the nth node from the end of a linked list
+first we iterate one place holder the nth time
+then we iterate the same place holder to the end as well as another place holder from the begining of the linkedlist at the same time
+this will result in the place holder that started from the end having nth distance from the end
+*/
