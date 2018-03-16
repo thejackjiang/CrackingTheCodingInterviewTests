ff37c28378800c0be261e8268d3078b000159d03
Jack Jiang <thejackjiang@gmail.com>
2018-03-16 06:10:40 -0400

    feat: comments for 8.1

--- src/Recursion and Dynamic Programming/8-1.js
+++ src/Recursion and Dynamic Programming/8-1.js
@@ -5,3 +5,9 @@ const steps = (n) => {
   }
   return step[n]
 }
+
+/*
+the first few number of steps is always 1, 1, 2, 4
+to find the nth step, it is the sum of the last 3 before n
+loop through to n and and fill out an array of n-1 length and return the nth element
+*/
